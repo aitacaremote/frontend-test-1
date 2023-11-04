8cf7c929fe13f58dccd856f867873a87ae685d74
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-11-04 08:19:18 +0100

    camera
    Installing React-camera-Pro and make  the component and his function.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,27 @@
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <div className="card">
-        <button>photo</button>
-      </div>
-    </>
-  );
-}
+import { useState, useRef } from "react";
+import { Camera } from "react-camera-pro";
 
-export default App;
+const Component = () => {
+  const camera = useRef<never>(null);
+  const [image, setImage] = useState<string | undefined>(undefined);
+
+  return (
+    <div>
+      <Camera ref={camera} />
+      <button
+        onClick={() => {
+          if (camera.current) {
+            setImage(camera.current.takePhoto());
+          }
+        }}
+      >
+        Take photo
+      </button>
+      {image && <img src={image} alt="Taken photo" />}
+    </div>
+  );
+};
+
+export default Component;
